9ccd5908202213b51375341b1f5e7cffe292d542
amer <amermostaafa@gmail.com>
2022-01-01 18:57:21 +0200

    commit

--- public/js/doc-view.js
+++ public/js/doc-view.js
@@ -1,7 +1,17 @@
 const deleteBtn = document.getElementById('delete');
 const deleteForm = document.getElementById('deleteForm');
-deleteBtn.addEventListener( 'click', ()=>{
-    if(confirm('Delete document ?')){
+deleteBtn.addEventListener('click', () => {
+    if (confirm('Delete document ?')) {
         deleteForm.submit();
     }
 })
+
+//socail share
+const doc = document.getElementById('myDoc');
+function fbs_click() {
+    u = doc.src;
+    // t=document.title;
+    t = doc.getAttribute('alt');
+    window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(u) + '&t=' + encodeURIComponent(t), 'sharer', 'toolbar=0,status=0,width=626,height=436');
+    return false;
+}
