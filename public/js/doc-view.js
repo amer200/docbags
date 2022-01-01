bdc21a312ab7b063f4082492db655e51dc1d81eb
amer <amermostaafa@gmail.com>
2022-01-01 19:35:13 +0200

    commit

--- public/js/doc-view.js
+++ public/js/doc-view.js
@@ -8,10 +8,14 @@ deleteBtn.addEventListener('click', () => {
 
 //socail share
 const doc = document.getElementById('myDoc');
+const twBtn = document.getElementById('tw-share');
 function fbs_click() {
     u = doc.src;
     // t=document.title;
     t = doc.getAttribute('alt');
     window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(u) + '&t=' + encodeURIComponent(t), 'sharer', 'toolbar=0,status=0,width=626,height=436');
     return false;
+}
+function twShare(){
+    return twBtn.href = `https://twitter.com/share?ref_src=${doc.src}`;
 }
